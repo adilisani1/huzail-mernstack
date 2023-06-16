97940a38e35c76b0ab949bcc136d33a07a2e5116
Muhammad Adil <adil.isani1@gmail.com>
2023-06-16 16:10:31 +0500

    new-bk

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user')
+const app = express();
+const port = 5000;
+const cors = require('cors');
+const complaintRoute = require('./routes/complaintRoute');
+
+mongoose.connect("mongodb+srv://adil:aadil%400321@cluster0.1ijveb9.mongodb.net/test")
+    .then(() => console.log("DBConnection Successfull"))
+    .catch((err) => console.log(err))
+
+
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/c", complaintRoute)
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`)
+});
